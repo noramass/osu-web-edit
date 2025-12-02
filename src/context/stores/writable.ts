1b94830bdf2f14fc96f5398848e8281741097f0d
Nora <nora@reichwald.me>
2025-12-02 02:33:51 +0100

    fix: api changes

--- src/context/stores/writable.ts
+++ src/context/stores/writable.ts
@@ -3,7 +3,7 @@ import { Writable, Subscriber, Unsubscriber, Invalidator } from "./types";
 
 
 export function writable<T>(initial?: T): Writable<T> & { get(): T } {
-  const { sub, unsub, update, set, get } = createStore(initial);
+  const { subscribe: sub, unsub, update, set, get } = createStore(initial);
 
   function subscribe(run: Subscriber<T>, invalidate?: Invalidator<T>): Unsubscriber {
     run(get());
